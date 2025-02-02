b360ca01bfca13b7ae013bbc9b5736b9a75576f1
Porramin <mosphonez3@gmail.com.com>
2025-02-02 11:55:23 +0700

    fixbug

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,7 +1,8 @@
 import azure.functions as func
-from flask import request, Flask
+from flask import request, Flask,jsonify
 from pymongo.mongo_client import MongoClient
-from flask_cors import CORS
+from bson.objectid import ObjectId
+from bson import json_util
 uri = "mongodb+srv://se1212312121:se1212312121@cluster0.kjvosuu.mongodb.net/"
 
 # Create a new client and connect to the server
@@ -11,6 +12,7 @@ app = Flask(__name__)
 
 db = client["vivart"]
 customer = db["customer"]
+post = db["post"]
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     """Each request is redirected to the WSGI handler.
@@ -37,3 +39,31 @@ def login():
     if find:
         return {"message": "login successful"}, 201
     return {"message": "login fail"}, 401
+
+@app.route("/post", methods=["POST"])
+def postdata():
+    data = request.get_json()
+    post.insert_one(data)
+    return {"message": "upload successful"}, 200
+
+
+@app.route('/post/<string:_id>', methods=['GET'])
+def getpost(_id):
+    object_id = ObjectId(_id)  # แปลง _id เป็น ObjectId
+    data = post.find_one({"_id": object_id})  # ค้นหาจาก MongoDB
+    if data:
+        data['_id'] = str(data['_id'])  # แปลง ObjectId เป็น string เพื่อให้ JSON ใช้ได้
+        return jsonify(data)
+    else:
+        return jsonify({"error": "Data not found"}), 404
+    
+@app.route('/post', methods=['GET'])
+def getallpost():
+    data = list(post.find())  # ดึงข้อมูลทั้งหมดจาก MongoDB
+    if data:
+        # แปลง ObjectId เป็น string ในทุก document
+        for item in data:
+            item['_id'] = str(item['_id'])  # แปลง ObjectId เป็น string
+        return jsonify(data)
+    else:
+        return jsonify({"error": "Data not found"}), 404
